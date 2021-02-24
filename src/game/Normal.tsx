bee5b4d87611ac69a5f81bf094b18405ed436a75
HamishWHC <hamishwhc@gmail.com>
2021-02-24 20:30:32 +1100

    Got animations working. But discovered ChessboardJSX. Going to switch to it.

--- src/game/Normal.tsx
+++ src/game/Normal.tsx
@@ -1,9 +1,24 @@
 import {START_BOARD} from "./index";
 import {Game, GameUiProps} from "./base";
-import {Side, Move, PieceType} from "./types";
-import {useBoard} from "./useBoard";
+import {Side, Move, PieceType, Board} from "./types";
+import {BoardWithIds, useBoard} from "./useBoard";
 import {Sides} from "./Sides";
 import React from "react"
+
+const allocatePieceIds = (board: Board, prev?: BoardWithIds, move?: Move): BoardWithIds => {
+    if (prev && move) {
+        let prevId = prev[move.start.y][move.start.x]!.id
+        return board.map((rank, y) => rank.map((p, x) => {
+            if (`${move.end.x}${move.end.y}` === `${x}${y}`) {
+                return p ? {...p, id: prevId} : null
+            }
+            return p ? {...p, id: prev[y][x]!.id} : null
+        }))
+    } else {
+        let uid = 0;
+        return board.map(rank => rank.map(p => p ? {...p, id: uid++} : null))
+    }
+}
 
 export class Normal extends Game {
     mode = "NORMAL"
@@ -14,32 +29,32 @@ export class Normal extends Game {
         return true
     }
     Ui = ({game, names}: GameUiProps) => {
-        const {view, activeSide, highlighted, setHighlighted} = useBoard({
-            game,
-            onActiveSidePieceClick: () => {},
-            onHighlightedPositionClick: () => {},
-            allocatePieceIds: (board, prev, move) => {
-                if (prev && move) {
-                    let prevId = prev[move.start.y][move.start.x]?.id
-                    return board.map((rank, y) => rank.map((p, x) => {
-                        if (prevId && `${move.end.x}${move.end.y}` === `${x}${y}`) {
-                            return p ? {...p, id: prevId} : null
-                        }
-                        return p ? {...p, id: prev[y][x]!.id} : null
-                    }))
-                } else {
-                    let uid = 0;
-                    return board.map(rank => rank.map(p => p ? {...p, id: uid++} : null))
-                }
-            }
+        const {view, activeSide, highlighted, setHighlighted, setBoard, setActiveSide} = useBoard({
+            initialBoard: allocatePieceIds(game.board),
+            initialTurn: game.turn,
+            onActiveSidePieceClick: (pos, _) => {
+                setHighlighted([pos])
+            },
+            onHighlightedPositionClick: () => {}
         })
+
         React.useEffect(() => {
-            setHighlighted([{x: 2, y: 4}])
+            const handleBoardChange = (move: Move) => {
+                setBoard(prev => allocatePieceIds(game.board, prev, move))
+                setActiveSide(game.turn)
+            }
+            game.events.on("move", handleBoardChange)
+            return () => {
+                game.events.off("move", handleBoardChange)
+            }
+        }, [game])
+
+        React.useEffect(() => {
             setTimeout(() => {
-                console.log("timeout running")
-                game.move({start: {x: 3, y: 6}, end: {x: 7, y: 0}})
+                game.move({start: {x: 4, y: 6}, end: {x: 4, y: 4}})
             }, 3000)
         }, [])
+    
         return <Sides active={{[activeSide === Side.WHITE ? "bottom" : "top"]: true}} names={{left: "HamishWHC"}}>
             {view}
         </Sides>
--- src/game/useBoard.tsx
+++ src/game/useBoard.tsx
@@ -4,58 +4,73 @@ import type {Board, Move, Piece, Position, Side} from "./types"
 import styles from "./Board.module.css"
 import {Flipped, Flipper} from "react-flip-toolkit"
 import type {Game} from "./base"
-
 interface UseBoardOptions {
-    game: Game
     onActiveSidePieceClick: (position: Position, piece: Piece) => void
     onHighlightedPositionClick: (position: Position) => void
-    allocatePieceIds: (board: Board, prev?: BoardWithIds, move?: Move) => BoardWithIds
+    initialBoard: BoardWithIds,
+    initialTurn: Side,
+    onAnimationComplete?: () => void
 }
 
-type BoardWithIds = (Piece & {id: number} | null)[][]
+export type BoardWithIds = (Piece & {id: number} | null)[][]
+
+const makeOnTop = (el: HTMLElement) => {
+    el.style.zIndex = "1000"
+}
+
+const resetZIndex = (el: HTMLElement) => {
+    el.style.zIndex = ""
+}
 
 export const useBoard = (options: UseBoardOptions) => {
-    let [board, setBoard] = React.useState(options.allocatePieceIds(options.game.board))
-    let [activeSide, setActiveSide] = React.useState(options.game.turn)
-
-    React.useEffect(() => {
-        const handleBoardChange = (move: Move) => {
-            console.log("handling board change")
-            setBoard(prev => options.allocatePieceIds(options.game.board, prev, move))
-            setActiveSide(options.game.turn)
-        }
-        options.game.events.on("move", handleBoardChange)
-        return () => {
-            options.game.events.off("move", handleBoardChange)
-        }
-    }, [options])
+    let [board, setBoard] = React.useState(options.initialBoard)
+    let [activeSide, setActiveSide] = React.useState(options.initialTurn)
 
     let [locked, setLocked] = React.useState(false)
     let [highlighted, setHighlighted] = React.useState<Position[]>([])
     const hlStrings = highlighted.map(p => `${p.x}${p.y}`)
 
+    const makeOnActiveSidePieceClick = React.useCallback((pos: Position, piece: Piece) => {
+        return () => options.onActiveSidePieceClick(pos, piece)
+    }, [options.onActiveSidePieceClick])
+
+    const makeOnHighlightedPositionClick = React.useCallback((pos: Position) => {
+        return () => options.onHighlightedPositionClick(pos)
+    }, [options.onHighlightedPositionClick])
+
     return {
         setActiveSide,
         setHighlighted,
         setLocked,
+        setBoard,
         activeSide,
         board,
         highlighted,
         locked,
-        view: <Flipper flipKey={JSON.stringify(board)}>
+        view: <Flipper flipKey={JSON.stringify(board)} onComplete={options.onAnimationComplete}>
             <div className={styles.board}>
                 {
                     board.flatMap((rank, y) => rank.map(
-                        (piece, x) => <div key={`${x}${y}`} className={styles.square}
+                        (piece, x) => <div
+                            key={`${x}${y}`}
+                            className={styles.square}
                             style={{
                                 gridRow: `${y + 1}`,
                                 gridColumn: `${x + 1}`,
                                 backgroundColor: x % 2 ^ y % 2 ? "#b58863" : "#f0d9b5"
-                            }}>
-                            {piece && <Flipped flipId={piece.id}>
-                                <img className={styles.piece} src={getPieceImgSrc(piece)} />
+                            }}
+                        >
+                            {piece && <Flipped flipId={piece.id} onStart={makeOnTop} onComplete={resetZIndex}>
+                                <img
+                                    className={styles.piece}
+                                    src={getPieceImgSrc(piece)}
+                                    onClick={!locked && piece.side === activeSide ? makeOnActiveSidePieceClick({x, y}, piece) : undefined}
+                                />
                             </Flipped>}
-                            {hlStrings.includes(`${x}${y}`) && <div className={styles.highlight} />}
+                            {hlStrings.includes(`${x}${y}`) && <div
+                                className={styles.highlight}
+                                onClick={!locked ? makeOnHighlightedPositionClick({x, y}) : undefined}
+                            />}
                         </div>
                     ))
                 }
